37eb773ac22b192721cc836c92cb70d76090c3fc
tanmayyb <tbishnoi@torontomu.ca>
2023-05-21 14:28:53 -0400

    pid consts

--- rover/settings/pid.py
+++ rover/settings/pid.py
@@ -4,16 +4,16 @@ Turnings for Miniwalk and Approach Controllers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 """
 
-MINIWALK_PID_CONTROL_CONSTS = [[0.6,  0.0, 0.1], 
-                                30, 0.0]                    # good for kerr QUAD GRASS
-MINIWALK_AUX_CONTROL_CONSTS = [20,                          #drift_const[pwm]
-                               [60.0, 11.0, 0.0]]           #boost(range[deg], peak[pwm], deadzone[deg]) 
-
 APPROACH_PID_CONTROL_CONSTS = [[0.3, 0.0, 0.1], 
                                 15, 20.0]
 APPROACH_AUX_CONTROL_CONSTS = [0, 
                                [60.0, 11.0, 0.0]] 
 
+
+MINIWALK_PID_CONTROL_CONSTS = [[0.6,  0.0, 0.1], 
+                                30, 0.0]                    # good for kerr QUAD GRASS
+MINIWALK_AUX_CONTROL_CONSTS = [20,                          #drift_const[pwm]
+                               [60.0, 11.0, 0.0]]           #boost(range[deg], peak[pwm], deadzone[deg]) 
 
 
 """
