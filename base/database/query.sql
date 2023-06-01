8670ddaa6822cd0691c7bf991117616d4adec4e1
Ryan Le <Ryanle.le@gmail.com>
2023-06-01 18:18:11 -0400

    updated database

---
+++ base/database/query.sql
@@ -0,0 +1,19 @@
+-- SQLite
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201478, 100791, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100791\201478.png'));
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201479, 100791, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100791\201479.png'));
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201478, 100792, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100792\201478.png'));
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201479, 100792, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100792\201479.png'));
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201478, 100793, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100793\201478.png'));
+
+INSERT INTO tiles (zoom, x, y, server, tile_image)
+VALUES (19, 201479, 100793, 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.org', readfile('C:\Users\vshad\OneDrive\Documents\Program\offline_test_map_db\19\100793\201479.png'));
